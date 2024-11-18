ab1cb404e10d245f494218780a7de2ca76136840
Red Häwk <128895420+Redhawk-12@users.noreply.github.com>
2024-11-18 18:21:54 +0530

    Update sessionmaker.py

--- sessionmaker.py
+++ sessionmaker.py
@@ -2,11 +2,11 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 
-API_KEY = int(input("INSERT YOUR API KEY-->"))
-API_HASH = input("INSERT YOUR API HASH-->") 
+API_KEY = int(input("20681550"))
+API_HASH = input("e7568e1e1100838d24d89a3a7bef305c") 
 
 
 bot = TelegramClient(StringSession(), API_KEY, API_HASH)
 bot.start()
 string_session = bot.session.save()
-print(string_session)
+print(1BVtsOJMBu7meqSLeNRpbPyr6XLlx7CxU1wvbou0D2lysV9RrACuDbu12pKyg_fIoTkkeQL8WyldCHVxUlNk3T4TZI0CmdJgZIURS9-N8F09GtaUoo8q1CnDxxtLTkQhyvxD3IkC_DhsuxEXY1e1_mEM730HKSQeCHvsBlZreFn_Zjn9u1ptmpLoYgZH2xPBzv4-Jh5YFmff0oKWe_afjmkoMCTitSkBCUcJGqrxLoPKCsd-EWilSRxOAS0HWgFRslWcKN3EgfTJCImdw6OkIeJPolfOX3F896D4ytF-qoY9p764u9Mke61wAHE1WwpXltKWnJ7GwBuW6mvUUux5c5NzcJuuP85U=)
